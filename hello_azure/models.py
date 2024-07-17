aab6618986a2c9b3e31514e7763d17fe0dba40da
Sogo Ito <166690823+Sogo-Ito-sti@users.noreply.github.com>
2024-07-17 15:53:58 +0900

    Update models.py

--- hello_azure/models.py
+++ hello_azure/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class MyModel(models.Model):
+    field1 = models.CharField(max_length=100)
+    field2 = models.IntegerField(default=20)
+    field3 = models.IntegerField(default=200)
+    # Add more fields as needed
+
+def __str__(self):
+        return f"{self.field1}({self.field2}・{self.field3})"
