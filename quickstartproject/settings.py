9a2681c191bbf090dc35d854f4d02cd326462bb3
Sogo Ito <166690823+Sogo-Ito-sti@users.noreply.github.com>
2024-07-17 14:51:40 +0900

    Update settings.py

--- quickstartproject/settings.py
+++ quickstartproject/settings.py
@@ -78,13 +78,22 @@ WSGI_APPLICATION = 'quickstartproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'recipenavitest-database',
+        'HOST': 'recipenavitest-server.mysql.database.azure.com',
+        'USER': 'fczklhqtur',
+        'PASSWORD': 'cskLAD2126',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
