7e8086bd1a51be6322e3dc3b0d03e093c5e0ee66
Sogo Ito <166690823+Sogo-Ito-sti@users.noreply.github.com>
2024-07-18 15:25:15 +0900

    Update views.py

--- hello_azure/views.py
+++ hello_azure/views.py
@@ -3,8 +3,12 @@ from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 
 def index(request):
+    # Azure から提供されるヘッダーから ID トークンを取得
+    id_token = request.META.get('HTTP_X_MS_TOKEN_GOOGLE_ID_TOKEN')
+    context = {'id_token': id_token}
+    
     print('Request for index page received')
-    return render(request, 'hello_azure/index.html')
+    return render(request, 'hello_azure/index.html', context)
 
 @csrf_exempt
 def hello(request):
